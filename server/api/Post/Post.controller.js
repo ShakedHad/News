436c7474d7a3139a31e841899cbcfb4f977d5827
Shir <shir.avraham13@gmail.com>
2020-01-11 19:55:59 +0200

    implemented get post by id

--- server/api/Post/Post.controller.js
+++ server/api/Post/Post.controller.js
@@ -1,5 +1,4 @@
 import Post from './Post.model';
-import User from '../User/User.model';
 
 export const getAllPosts = async (req, res) => {
     try {
@@ -36,17 +35,16 @@ export const editUser = (req, res) => {
     res.status(501).send('not implemented');
 };
 
-export const getUser = async (req, res) => {
+export const getPost = async (req, res) => {
     try {
-        const user = await User.findById(req.params.id);
-        console.log(user);
+        const post = await Post.findById(req.params.id);
 
         // Check for ObjectId format and post
-        if (!req.params.id.match(/^[0-9a-fA-F]{24}$/) || !user) {
+        if (!req.params.id.match(/^[0-9a-fA-F]{24}$/) || !post) {
             return res.status(404).json({ msg: 'User not found' });
         }
 
-        res.json(user);
+        res.json(post);
     } catch (err) {
         console.error(err.message);
 
