c8fb87d79eed7bc057bbba5cf39fcb36e326155f
shiroosh <shir.avraham13@Gmail.com>
2020-01-11 20:53:33 +0200

    Revert "Feat/index page"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,33 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true
-  },
-  extends: ['plugin:react/recommended', 'airbnb'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    env: {
+        browser: true,
+        es6: true
     },
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
-  plugins: ['react', 'react-hooks'],
-  rules: {
-    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
-    'linebreak-style': 'off',
-    'react/jsx-filename-extension': 'off',
-    'no-console': 'off',
-    indent: ['error', 4],
-    'eol-last': 'off',
-    'import/prefer-default-export': 'off',
-    'react/jsx-indent': 'off',
-    'comma-dangle': 'off',
-    'consistent-return': 'off',
-    'object-curly-newline': 'off',
-    "arrow-body-style": "off",
-  }
+    extends: ['plugin:react/recommended', 'airbnb'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+    plugins: ['react', 'react-hooks'],
+    rules: {
+        'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+        'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+        'linebreak-style': 'off',
+        'react/jsx-filename-extension': 'off',
+        'no-console': 'off',
+        indent: ['error', 4],
+        'eol-last': 'off',
+        'import/prefer-default-export': 'off',
+        'react/jsx-indent': 'off',
+        'comma-dangle': 'off',
+        'consistent-return': 'off',
+        'object-curly-newline': 'off'
+    }
 };
--- server/api/Post/Post.controller.js
+++ server/api/Post/Post.controller.js
@@ -1,53 +1,15 @@
-import Post from './Post.model';
+export const getAllPosts = () => {
 
-export const getAllPosts = async (req, res) => {
-    try {
-        const posts = await Post.find();
-        res.json(posts);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
 };
 
-export const addPost = async (req, res) => {
-    try {
-        const newPost = new Post({
-            title: req.body.title,
-            text: req.body.text,
-            artist: req.body.artist,
-            price: req.body.price,
-            user: req.body.userId,
-        });
+export const addPost = () => {
 
-        console.log(newPost.date);
-
-        const post = await newPost.save();
-
-        res.json(post);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
 };
 
-export const editUser = (req, res) => {
-    res.status(501).send('not implemented');
+export const editPosts = () => {
+
 };
 
-export const getPost = async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
+export const getPost = () => {
 
-        // Check for ObjectId format and post
-        if (!req.params.id.match(/^[0-9a-fA-F]{24}$/) || !post) {
-            return res.status(404).json({ msg: 'User not found' });
-        }
-
-        res.json(post);
-    } catch (err) {
-        console.error(err.message);
-
-        res.status(500).send('Server Error');
-    }
 };
