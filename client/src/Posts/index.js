896121ad5807f1ed18a9874ac25938fd0d97fc26
Shir <shir.avraham13@gmail.com>
2020-01-11 20:50:40 +0200

    improved post card

--- client/src/Posts/index.js
+++ client/src/Posts/index.js
@@ -25,34 +25,25 @@ const useStyles = makeStyles({
 
 export default function Post() {
     const classes = useStyles();
-    const bull = <span className={classes.bullet}>•</span>;
 
     return (
         <Card className={classes.card}>
             <CardContent>
-                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                    Word of the Day
-                </Typography>
                 <Typography variant="h5" component="h2">
-                    be
-                    {bull}
-                    nev
-                    {bull}
-                    o
-                    {bull}
-                    lent
+                    Tickets to Aerosmith
                 </Typography>
                 <Typography className={classes.pos} color="textSecondary">
-                    adjective
+                    27/02/2020
                 </Typography>
                 <Typography variant="body2" component="p">
-                    well meaning and kindly.
-                    <br />
-                    a benevolent smile
+                    Hi, I'm selling tickets to the aerosmith concert
+                </Typography>
+                <Typography align="right">
+                    250$
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small">Learn More</Button>
+                <Button size="small">More Details</Button>
             </CardActions>
         </Card>
     );
