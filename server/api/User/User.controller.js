fd18dc759805f8c7a3b9388f9d334e2a06d9bd64
Shir <shir.avraham13@gmail.com>
2020-01-11 14:42:24 +0200

    added user model & controller

---
+++ server/api/User/User.controller.js
@@ -0,0 +1,11 @@
+export const getAllUsers = () => { };
+
+export const addUser = () => { };
+
+export const editUser = (id) => {
+    console.log(id);
+};
+
+export const getUser = (id) => {
+    console.log(id);
+};
