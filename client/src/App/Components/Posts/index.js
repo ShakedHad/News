c6fe3bc6121e45ddc949720e8b5fe7ca21477cd5
ShakedHad <shakedh100@gmail.com>
2020-01-15 19:29:39 +0200

    added axios

--- client/src/App/Components/Posts/index.js
+++ client/src/App/Components/Posts/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
 
 const useStyles = makeStyles({
     card: {
@@ -25,6 +26,15 @@ const useStyles = makeStyles({
 
 export default function Post() {
     const classes = useStyles();
+
+    useEffect(() => {
+        (() => {
+            console.log('effect happened');
+        })();
+
+    });
+
+    axios.get('/api/posts').then(data => console.log(data));
 
     return (
         <Card className={classes.card}>
