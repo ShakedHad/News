c4d4f8e8bb4a42feb1256c638e2a6351cd76c060
Shaked Hadas <shakedh100@gmail.com>
2020-07-17 23:26:00 +0300

    make the server serve the client and get ready to deploy to production

--- server/config/express.js
+++ server/config/express.js
@@ -1,16 +1,19 @@
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path';
 import router from '../api';
 
 export default () => {
     const app = express();
-
-    app.use('/static', express.static('../bundle'));
     app.use('/assets', express.static('assets'));
     app.use(morgan('dev', { immediate: true }));
     app.use(express.json());
-
+    
+    app.use('/public', express.static('./dist/public'));
     app.use('/api', router);
+    app.get('/', (req, res) => {
+        res.redirect('/public/index.html');
+    });
 
     app.listen(process.env.HOST_PORT, () => console.log(`Listening on port ${process.env.HOST_PORT}`));
 };
